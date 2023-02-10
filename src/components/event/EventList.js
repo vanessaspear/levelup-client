52cd1f3b127ed8f828d9106a118e814df172a482
Vanessa Spear <vanessavspear@gmail.com>
2023-02-10 10:57:12 -0600

    Added join and leave functions and buttons to handle a user joining and leaving an event.

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { getEvents, deleteEvent } from "../../managers/EventManager"
+import { getEvents, deleteEvent, joinEvent, leaveEvent } from "../../managers/EventManager"
 import { useNavigate } from "react-router-dom"
 
 export const EventList = (props) => {
@@ -22,17 +22,32 @@ export const EventList = (props) => {
                 events.map(event => {
                     return <div key={`event--${event.id}`}>
                         <>
+                            <section className="event">
+                                <div className="event__title">{event.title} by {event.gamer.full_name}</div>
+                                <div className="event__players">{event.game.num_players} players needed</div>
+                                <div className="event__skillLevel">Skill level is {event.game.skill_level}</div>
+                            </section>
+                            {
+                                event.joined ? 
+                                <button
+                                    onClick={() => { 
+                                        leaveEvent(event.id)
+                                        navigate(0)
+                                    }}
+                                >Remove Event</button>
+                                :  <button
+                                    onClick={() => { 
+                                        joinEvent(event.id)
+                                        navigate(0)
+                                    }}
+                                >Sign Up for Event</button>
+                            }
                             <button
                                 onClick={() => { 
                                     deleteEvent(event.id) 
                                     navigate(0)
                                 }}
                             >Delete Event</button>
-                            <section className="event">
-                                <div className="event__title">{event.title} by {event.gamer.full_name}</div>
-                                <div className="event__players">{event.game.num_players} players needed</div>
-                                <div className="event__skillLevel">Skill level is {event.game.skill_level}</div>
-                            </section>
                         </> 
                     </div>
                 })
--- src/managers/EventManager.js
+++ src/managers/EventManager.js
@@ -46,3 +46,26 @@ export const deleteEvent = (eventId) => {
         }
     })
 }
+
+export const leaveEvent = (eventId) => {
+    // TODO: Write the DELETE fetch request to leave an event
+    return fetch(`http://localhost:8000/events/${eventId}/leave`, {
+        method: "DELETE",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+  }
+  
+  export const joinEvent = (eventId) => {
+      // TODO: Write the POST fetch request to join and event
+      return fetch(`http://localhost:8000/events/${eventId}/signup`, {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+        .then(getEvents)
+  }
